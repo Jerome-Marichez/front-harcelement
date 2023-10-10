37baed3507f2977cce588a70286ae57def984013
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-10-10 16:09:44 +0200

    book maj

--- sexeduc/src/components/books/books.jsx
+++ sexeduc/src/components/books/books.jsx
@@ -1,20 +1,28 @@
 import "./book.css";
 import { useState } from "react";
 
-export function Books({isOpen}) {
-	
-	return (
-	<div className="books">
-		<div className="close">X</div>
-		<div className="text">
-			<p>Livre de sciences</p>
-			<p>Livre de bricolage</p>
-			<p>Livre de cuisine</p>
-			<p>Livre de jus orange</p>
-			<p>Livre de jardinage</p>
-			<p>Livre sur la robotique</p>
-			<p>Livre sur les bananes</p>
-		</div>
-	</div>
-	)
+export function Books({ isOpen }) {
+	const [bookOpen, setBookOpen] = useState(true);
+
+	if (bookOpen) {
+		return (
+			<div className="books">
+				<div className="close" onClick={() => setBookOpen(false)}>X</div>
+				<div className="text">
+					<p>Livre de sciences</p>
+					<p>Livre de bricolage</p>
+					<p>Livre de cuisine</p>
+					<p>Livre de jus orange</p>
+					<p>Livre de jardinage</p>
+					<p>Livre sur la robotique</p>
+					<p>Livre sur les bananes</p>
+				</div>
+			</div>
+		)
+	}
+
+	if (!bookOpen) {
+		return (<></>);
+	}
+
 }
--- sexeduc/src/game.js
+++ sexeduc/src/game.js
@@ -6,12 +6,13 @@ import { Object } from "./components/object/object.jsx";
 import { useState } from "react";
 import { MessageText } from "./components/messageText/messageText";
 import { Books } from "./components/books/books";
+
 export function Game() {
 	const [text, setText] = useState("Bonjour vous êtes le nouveau gardien(e) de l'entreprise malheureusement les disjoncteurs ont peter les plombs vous devez trouvez la clé manuel, votre précédesseur l'a malheureusement oubliez de vous indiquez le mot de passe de son cassier ou ce trouve la clé, trouvez les indices qui vous permetterons de la récuperer");
 	const selectedLevel = levels.find((level) => level.title_niveau === "level1");
 	const objects = selectedLevel.objects;
 
-
+	
 	return (
 		<div className="game">
 			<Level map={selectedLevel.map} nightmode={true}>
@@ -26,7 +27,7 @@ export function Game() {
 			</Level>
 
 			<MessageText text={text} setText={setText} />
-			<Books isOpen={true}/>
+			<Books isOpen={false}/>
 		</div>
 	);
 }
