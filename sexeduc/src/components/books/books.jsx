b1b9bbc538305c6983ea6e4a2856c0eaf5be6e50
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-10-10 15:55:22 +0200

    v3-maj

---
+++ sexeduc/src/components/books/books.jsx
@@ -0,0 +1,20 @@
+import "./book.css";
+import { useState } from "react";
+
+export function Books({isOpen}) {
+	
+	return (
+	<div className="books">
+		<div className="close">X</div>
+		<div className="text">
+			<p>Livre de sciences</p>
+			<p>Livre de bricolage</p>
+			<p>Livre de cuisine</p>
+			<p>Livre de jus orange</p>
+			<p>Livre de jardinage</p>
+			<p>Livre sur la robotique</p>
+			<p>Livre sur les bananes</p>
+		</div>
+	</div>
+	)
+}
